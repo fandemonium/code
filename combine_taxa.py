15c29e65ed017d19e6f87f0b6f2b8bd67a9f04d0
FY <snisiarc@gmail.com>
2014-04-03 13:28:56 -0500

    edited the usage of combine_taxa.py

--- combine_taxa.py
+++ combine_taxa.py
@@ -1,7 +1,7 @@
 import sys
 import os
 
-##~/Documents/Fan/scratch/parsed_files/combined$ for i in *.combined; do python ~/Documents/Fan/scratch/code/combine_taxa.py ~/Documents/Databases/uniprotKB_id_prok.txt $i > ~/Documents/Fan/scratch/parsed_files/kingdom_combined/$i.kingdom; done
+##~/Documents/Fan/scratch/parsed_files/combined$ for i in *.combined; do python ~/Documents/Fan/code/combine_taxa.py ~/Documents/Databases/uniprotKB_id_prok.txt $i > ~/Documents/Fan/scratch/parsed_files/kingdom_combined_no_restriction/$i.kingdom; done 
 
 dict = {}
 for line in open(sys.argv[1]):
