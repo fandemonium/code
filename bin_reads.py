e6f2e0741770f234557911339c6e0e4a25912d49
FY <snisiarc@gmail.com>
2014-10-19 14:47:03 -0500

    modified with os.walk

--- bin_reads.py
+++ bin_reads.py
@@ -1,31 +1,53 @@
 ##########################################################################################
 ## to use in binning pandakiller assembled samples into seqfilter parsed tag directories############################################################################################
-
+import os
 import sys
 from Bio import SeqIO
-
-names = []
-tags = open(sys.argv[1], 'rU')
-for record in SeqIO.parse(tags, "fasta"):
-	names.append(record.id.split(' ')[0])
-#print names
+import string
 
 ids = []
 reads = []
-assemb = open(sys.argv[2], 'rU')
+assemb = open(sys.argv[1], 'rU')
 for line in SeqIO.parse(assemb, "fastq"):
-	lexemes = line.id.rsplit(':', 1)
+        lexemes = line.id.rsplit(':', 1)
 	ids.append(lexemes[0])
-	reads.append(line.seq)
-
+        reads.append(line.seq)
 dict_assemb = dict(zip(ids, reads))
 
-output = open(sys.argv[3], 'w')
-drop = open(sys.argv[4], 'w')
-for item in names:
-	if item in dict_assemb:	
-		output.write('>%s\n' % (item))
-		output.write('%s\n' % (dict_assemb[item]))
-	else:
-		drop. write('%s\n' % (item))
-	
+rootdir = "."
+lfiles = []
+for topdir, subdir, filelist in os.walk(rootdir):
+	for files in filelist:
+		if files.endswith("trimmed.fasta"):
+			lfiles.append(files)
+
+for each in lfiles:
+	fout = '%s_assem.fasta' % each.strip()
+        fp = open(fout, 'w')
+	f = open(each, 'rU')
+	l = []
+	for records in SeqIO.parse(f, 'fasta'):
+		name = records.id.split(' ')[0]
+		if name in dict_assemb:
+			#print each + ':' + name
+			l.append(name)
+#	print len(l)
+	for item in l:
+		fp.write('>%s\n' % item) 
+		fp.write('%s\n' % dict_assemb[item])
+#	for files in filelist:
+#		if files.endswith("trimmed.fasta"):
+#			lfiles.append(files)
+#			for each in lfiles:
+#				outfiles_keep = open(each + '_' + 'assem' + '.fasta', 'w')
+				#outfiles_drop = open(each + '_' + 'dropped' + 'reads' + '.txt', 'w')
+#				f = open(each, 'rU')
+#				names = []
+#				for records in SeqIO.parse(f, 'fasta'):
+#					names.append(records.id.split(' ')[0])
+#				for item in names:
+#					if item in dict_assemb:	
+#						outfiles_keep.write('>%s\n' % (item))
+#						outfiles_keep.write('%s\n' % (dict_assemb[item]))
+					#else:
+					#	outfiles_drop. write('%s\n' % (item))
