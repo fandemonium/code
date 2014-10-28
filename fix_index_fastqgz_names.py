121246971c20f407d0875cae1638c8673b0a57a6
FY <snisiarc@gmail.com>
2014-10-28 16:57:05 -0500

    add usage

--- fix_index_fastqgz_names.py
+++ fix_index_fastqgz_names.py
@@ -1,8 +1,12 @@
-##fixing index fastq.gz seq names to match pandakiller's seq names###
+##fixing index fastq.gz seq names to match RDP assembler's seq names###
 
 import sys
 import gzip
 from Bio import SeqIO
+
+if len(sys.argv) != 3:
+	print "USAGE: python fix_index_fastqgz_names.py XXX_I.fastq.gz FIXED_I.fastq"
+	sys.exit()
 
 f = gzip.open(sys.argv[1], 'rU')
 output = open(sys.argv[2], 'w')
