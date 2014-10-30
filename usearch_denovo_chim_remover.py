c22ca3942a4ae8ead2b519f0cee8d48dfab1be5d
FY <snisiarc@gmail.com>
2014-10-30 10:54:49 -0500

    add sample id to each seq

--- usearch_denovo_chim_remover.py
+++ usearch_denovo_chim_remover.py
@@ -3,9 +3,10 @@ import sys
 from Bio import SeqIO
 
 out = open(sys.argv[2], 'w')
+#print sys.argv[1]
 for records in SeqIO.parse(open(sys.argv[1], 'rU'), "fasta"):
 	up = records.id.split(';')[-2]
-	records.id = records.id.split(';')[0] + ';' + records.id.split(';')[1]
+	records.id = records.id.split(';')[0] + ';' + records.id.split(';')[1] + ';barcodelabel=%s' % sys.argv[1]
 	records.name = records.id
 	records.description = records.id
 	if up != "up=chimera":
