edfcd9ff3bd4b0316f939a0cff7eef4f751ad831
FY <snisiarc@gmail.com>
2014-10-31 00:08:26 -0500

    muted adding sample names

--- usearch_denovo_chim_remover.py
+++ usearch_denovo_chim_remover.py
@@ -6,7 +6,7 @@ out = open(sys.argv[2], 'w')
 #print sys.argv[1]
 for records in SeqIO.parse(open(sys.argv[1], 'rU'), "fasta"):
 	up = records.id.split(';')[-2]
-	records.id = records.id.split(';')[0] + ';' + records.id.split(';')[1] + ';barcodelabel=%s' % sys.argv[1]
+	records.id = records.id.split(';')[0] + ';' + records.id.split(';')[1]# + ';barcodelabel=%s' % sys.argv[1].split('.')[0]
 	records.name = records.id
 	records.description = records.id
 	if up != "up=chimera":
