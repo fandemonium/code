f2078c3bde7aa0322271d0730bba2c6d44ccadeb
Fan Yang <snisiarc@gmail.com>
2015-09-28 06:48:56 +0000

    added tools, seq length py script, modified uniprot dat parser

---
+++ length_summary.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+from Bio import SeqIO
+import sys
+import os
+
+if len(sys.argv) < 2:
+	print "USAGE: avg_lengths.py <infile>..."
+	sys.exit(1)
+
+length_map = dict()
+for f in sys.argv[1:]:
+	length_map[f] = []
+
+	for seq in SeqIO.parse(open(f), "fasta"):
+		length_map[f].append(len(seq.seq))
+
+longest_length = 0
+for f in length_map:
+	sys.stdout.write("%s\t" % os.path.split(f)[1])
+	length_map[f] = sorted(length_map[f])
+	if len(length_map[f]) > longest_length:
+		longest_length = len(length_map[f])
+
+sys.stdout.write("\n")
+
+for i in range(longest_length):
+	for l in length_map.values():
+		if len(l) > i:
+			sys.stdout.write("%s\t" % l[i])
+		else:
+			sys.stdout.write(" \t")
+
+	sys.stdout.write("\n")
--- uniprot_dat.py
+++ uniprot_dat.py
@@ -7,14 +7,19 @@ from Bio import SeqIO
 
 uniprot = open(sys.argv[1], "rU")
 
+taxa_to_print=[]
 for record in SeqIO.parse(uniprot, "swiss"):
-	print record
+#	print record
 #	id = record.id
-#	taxa = record.annotations['taxonomy']
+	taxa_to_print.append(sys.argv[1].split(".")[0])
+	taxa_to_print.append(record.annotations['organism'])
+	for taxa in record.annotations['taxonomy']:
+		taxa_to_print.append(taxa)
+print '\t'.join(taxa_to_print)
 	#if record.annotations['taxonomy'] >1:
 	#	domain = record.annotations['taxonomy'][1]
 	#	if kingdom != 'Eukaryota':
 	#		print "%s\t%s" % (id, kingdom)
 	#	else:
 	#		if domain == 'Fungi':
-#	print "%s\t%s" % (id, taxa)
+#	print '\t'.joint(strain, taxa_to_print)
