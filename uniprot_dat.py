7cd0dcccd651a539dc4637cd5e7c353986a8fcf5
Fan Yang <fanyang@fans-mbp.eeob.iastate.edu>
2015-09-29 12:20:56 -0500

    wrote get taxa into a function, removed fungi code from before

--- uniprot_dat.py
+++ uniprot_dat.py
@@ -5,21 +5,28 @@ import os
 from Bio import SeqIO
 #import gzip
 
-uniprot = open(sys.argv[1], "rU")
+#This script uses biopython to get taxonomy from uniprot .dat file
+'''
+record.annotations         record.letter_annotations
+record.dbxrefs             record.lower
+record.description         record.name
+record.features            record.reverse_complement
+record.format              record.seq
+record.id                  record.upper
+'''
 
-taxa_to_print=[]
-for record in SeqIO.parse(uniprot, "swiss"):
-#	print record
-#	id = record.id
-	taxa_to_print.append(sys.argv[1].split(".")[0])
-	taxa_to_print.append(record.annotations['organism'])
-	for taxa in record.annotations['taxonomy']:
-		taxa_to_print.append(taxa)
-print '\t'.join(taxa_to_print)
-	#if record.annotations['taxonomy'] >1:
-	#	domain = record.annotations['taxonomy'][1]
-	#	if kingdom != 'Eukaryota':
-	#		print "%s\t%s" % (id, kingdom)
-	#	else:
-	#		if domain == 'Fungi':
-#	print '\t'.joint(strain, taxa_to_print)
+def get_taxa(uniprot_dat):
+	taxa_to_print=[]
+	for record in SeqIO.parse(uniprot_dat, "swiss"):
+#		taxa_to_print.append(record.id)
+		taxa_to_print.append(uniprot_dat.split(".")[0])
+		taxa_to_print.append(record.annotations['organism'])
+		for taxa in record.annotations['taxonomy']:
+			taxa_to_print.append(taxa)
+	return '\t'.join(taxa_to_print)
+
+uniprot_dat = sys.argv[1:]
+
+for f in uniprot_dat:
+	taxa = get_taxa(f)
+	print taxa
