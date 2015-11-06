6a722b609658eb72b6d153627df216f06ca59f0a
Ubuntu <ubuntu@doe-mem-12.novalocal>
2015-11-06 15:08:48 +0000

    modified scripts needed for pitfoaming seq analysis

--- automation/rdp_otu_pipeline.sh
+++ automation/rdp_otu_pipeline.sh
@@ -6,17 +6,17 @@ LOCATION="/home/ubuntu/data/data3/fan/pitfoaming"
 #MAP="/Users/metagenomics/Documents/2014_COBS_Spruce_cdna/0_original/rdp_mapping_file_rev.txt"
 #FAN_PY="/Users/metagenomics/Documents/Fan/code"
 #UPARSE_PY="/Users/metagenomics/python_scripts"
-#REF_16S="/Users/metagenomics/Documents/Databases/RDPClassifier_16S_trainsetNo10_rawtrainingdata/trainset10_082014_rmdup.fasta"
+REF_16S="/mnt/data3/fan/RDPinfernal1.1Traindata/RDPClassifier_16S_trainsetNo15_rawtrainingdata/trainset15_092015.fa"
 MODEL_16S="/home/ubuntu/data/data3/fan/RDPinfernal1.1Traindata/bacteria_model.cm"
 
 SEQFILTER="/home/ubuntu/RDPTools/SeqFilters.jar"
-#PANDASEQ="/Users/metagenomics/RDP_Assembler/pandaseq/pandaseq"
-#USEARCH="/Users/metagenomics/usearch70"
-#CMALIGN="/Users/metagenomics/infernal1.1.1/bin/cmalign"
+PANDASEQ="/home/ubuntu/RDP_Assembler/pandaseq/pandaseq"
+USEARCH="/home/ubuntu/usearch"
+CMALIGN="/usr/local/bin/cmalign"
 ALIGNMENTTOOLS="/home/ubuntu/RDPTools/AlignmentTools.jar"
 CLUST="/home/ubuntu/RDPTools/Clustering.jar"
 CLASSIFIER="/home/ubuntu/RDPTools/classifier.jar"
-#FASTTREE="/Users/metagenomics/FastTree"
+FASTTREE="/home/ubuntu/FastTree"
 
 READQ="25"
 DIST="0.03"
@@ -26,18 +26,22 @@ BOOTSTRAP_VALUE="0.5"
 cd $LOCATION
 
 #echo "assembling pair-ended reads ..."
-#mkdir 1_"$NAME"_demultiplex 1_"$NAME"_demultiplex/assembled
-#
+#mkdir 1_"$NAME"_demultiplex 1_"$NAME"_demultiplex/assembled 1_"$NAME"_demultiplex/stats
 #$PANDASEQ -N -o 10 -e $READQ -F -d rbfkms -l 250 -L 280 -f $READ_1 -r $READ_2 1> 1_"$NAME"_demultiplex/assembled/assembled_reads.fastq 2>  1_"$NAME"_demultiplex/assembled/assembled_reads_stats.txt
+##bash /mnt/data3/fan/code/automation/pandaseq_assembler.sh /mnt/data3/fan/pitfoaming/sample_names_unique.txt
+##echo "pairend complated."
+##find pool_* -type f -size -700 -delete
+##mv pool_*/*.fastq 1_"$NAME"_demultiplex/assembled
+#mv pool_*/*stats.txt 1_"$NAME"_demultiplex/stats
 #echo "done."
-#
+
 #echo "parsing index file ..."
 #java -jar $SEQFILTER -Q $READQ --seq-file $INDEX_FILE --tag-file $MAP --outdir 1_"$NAME"_demultiplex/parse_index
 #echo "moving quality trimmed index reads to directory 'trimmed_tags'"
 #mkdir 1_"$NAME"_demultiplex/parse_index/trimmed_tags
 #mv 1_"$NAME"_demultiplex/parse_index/result_dir/*/*_trimmed.fasta 1_"$NAME"_demultiplex/parse_index/trimmed_tags/
 #echo "done."
-#
+
 #echo "demultiplexing assembled reads ..."
 #cd 1_"$NAME"_demultiplex/parse_index/trimmed_tags
 #mv NoTag_trimmed.fasta $LOCATION/1_"$NAME"_demultiplex/parse_index/result_dir/NoTag
@@ -46,97 +50,65 @@ cd $LOCATION
 #mkdir 1_"$NAME"_demultiplex/demultiplexed_assembled_reads
 #mv 1_"$NAME"_demultiplex/parse_index/trimmed_tags/*_assem.fastq 1_"$NAME"_demultiplex/demultiplexed_assembled_reads
 #echo "done."
-#
-#echo "prune reads to maxee=0.5 ..."
-#mkdir 2_"$NAME"_uparse 2_"$NAME"_uparse/quality_filtered
-#cd 1_"$NAME"_demultiplex/demultiplexed_assembled_reads
-#for i in *.fastq; do $USEARCH -fastq_filter $i -fastq_maxee 0.5 -fastaout $LOCATION/2_"$NAME"_uparse/quality_filtered/"$i"_maxee_0.5.fasta; done
-#echo "done."
-#
+
 #echo "dereplicate reads ..."
-#mkdir $LOCATION/2_"$NAME"_uparse/derep
-#cd $LOCATION/2_"$NAME"_uparse/quality_filtered
-#for i in *_0.5.fasta; do $USEARCH -derep_fulllength $i -output $LOCATION/2_"$NAME"_uparse/derep/"$i"_unique.fasta -sizeout; done
+#mkdir $LOCATION/2_"$NAME"_uchime $LOCATION/2_"$NAME"_uchime/derep
+#cd 1_"$NAME"_demultiplex/assembled
+#java -Xmx24g -jar $CLUST derep --unaligned -o $LOCATION/2_"$NAME"_uchime/derep/all_seqs_unique.fasta $LOCATION/2_"$NAME"_uchime/derep/all_seqs.ids $LOCATION/2_"$NAME"_uchime/derep/all_seqs.samples *.fastq
 #echo "done."
-#
-#echo "sorting by cluster size and removing singltons ..."
-#mkdir $LOCATION/2_"$NAME"_uparse/sorted
-#cd $LOCATION/2_"$NAME"_uparse/derep
-#for i in *_unique.fasta; do $USEARCH -sortbysize $i -output $LOCATION/2_"$NAME"_uparse/sorted/"$i"_sorted.fa -minsize 2; done
+
+#echo "remove extra space in RDPTools dereped reads ..."
+#cd $LOCATION/2_"$NAME"_uchime/derep
+#cat all_seqs_unique.fasta | tr -d ' ' > all_seqs_unique_nospace.fasta
+
+#echo "sorting by cluster size ..."
+#cd $LOCATION/2_"$NAME"_uchime
+#$USEARCH -sortbysize derep/all_seqs_unique_nospace.fasta -fastaout all_seqs_unique_nospace_sorted.fasta -minsize 1 > $LOCATION/log.txt
 #echo "done."
-#
+
 #echo "removing chimeras using de novo ..."
-#mkdir $LOCATION/2_"$NAME"_uparse/chime_denovo
-#cd $LOCATION/2_"$NAME"_uparse/sorted
-#for i in *.fa; do $USEARCH -cluster_otus $i -otuid 0.985 -otus $LOCATION/2_"$NAME"_uparse/chime_denovo/"$i"_otu1.fa; done
+#mkdir $LOCATION/2_"$NAME"_uchime/chime_denovo
+#$USEARCH -uchime_denovo all_seqs_unique_nospace_sorted.fasta -chimeras chime_denovo/all_seqs_unique_nospace_sorted.denovo.chimeras -nonchimeras chime_denovo/all_seqs_unique_nospace_sorted.denovo.good.fasta >> $LOCATION/log.txt
 #echo "done."
-#
+
 #echo "removing chimeras using references ..."
-#mkdir $LOCATION/2_"$NAME"_uparse/chime_ref $LOCATION/2_"$NAME"_uparse/chime_ref/stats $LOCATION/2_"$NAME"_uparse/chime_ref/chimeras $LOCATION/2_"$NAME"_uparse/chime_ref/good_otus
-#cd $LOCATION/2_"$NAME"_uparse/chime_denovo
-#for i in *_otu1.fa; do $USEARCH -uchime_ref $i -db $REF_16S -uchimeout $LOCATION/2_"$NAME"_uparse/chime_ref/stats/"$i".uchime -strand plus -selfid -mindiv 1.5 -mindiffs 5 -chimeras $LOCATION/2_"$NAME"_uparse/chime_ref/chimeras/"$i"_chimera.fa -nonchimeras $LOCATION/2_"$NAME"_uparse/chime_ref/good_otus/"$i"_good.fa; done
+#mkdir $LOCATION/2_"$NAME"_uchime/chime_ref
+#$USEARCH -uchime_ref chime_denovo/all_seqs_unique_nospace_sorted.denovo.good.fasta -db $REF_16S -uchimeout chime_ref/all_seqs_unique_nospace_sorted.ref.uchime -strand plus -chimeras chime_ref/all_seqs_unique_nospace_sorted.ref.chimera -nonchimeras chime_ref/all_seqs_unique_nospace_sorted.ref.good.fasta >> $LOCATION/log.txt
+#sed -i -e 's/;size/  ;size/g' chime_ref/all_seqs_unique_nospace_sorted.ref.good.fasta
 #echo "done."
-#
-#echo "renaming otus that are rid of chimeras to 'OTU_XX' ..."
-#mkdir $LOCATION/2_"$NAME"_uparse/chime_ref/good_otus_renamed
-#cd $LOCATION/2_"$NAME"_uparse/chime_ref/good_otus
-#for i in *_good.fa; do python $UPARSE_PY/fasta_number.py $i OTU_ > $LOCATION/2_"$NAME"_uparse/chime_ref/good_otus_renamed/"$i"_otu2.fa; done
-#echo "done"
-#
-#echo "mapping all sequences (including singletons) back to good OTU's of individual samples ..."
-#mkdir $LOCATION/2_"$NAME"_uparse/mapped $LOCATION/2_"$NAME"_uparse/mapped/uc $LOCATION/2_"$NAME"_uparse/mapped/seqs
-#cd 2_"$NAME"_uparse/quality_filtered
-#for i in *.5.fasta; do $USEARCH  -usearch_global $i -db $LOCATION/2_"$NAME"_uparse/chime_ref/good_otus_renamed/"$i"_unique.fasta_sorted.fa_otu1.fa_good.fa_otu2.fa -strand plus -id 0.985 -uc $LOCATION/2_"$NAME"_uparse/mapped/uc/"$i"_map.uc -matched $LOCATION/2_"$NAME"_uparse/mapped/seqs/"$i"_matched.fa; done
-#echo "done"
-#
-#echo "prepare finalized sequences for alignment and clustering ..."
-#echo "cancatenate and dereplicate ..."
-#mkdir $LOCATION/3_"$NAME"_alignment_and_cluster $LOCATION/3_"$NAME"_alignment_and_cluster/Derep_alignment
-#cd $LOCATION/good_seqs
-#java -Xmx16g -jar $CLUST derep --unaligned -o $LOCATION/3_"$NAME"_alignment_and_cluster/Derep_alignment/all_seqs_derep.fasta $LOCATION/3_"$NAME"_alignment_and_cluster/Derep_alignment/all_seqs.ids $LOCATION/3_"$NAME"_alignment_and_cluster/Derep_alignment/all_seqs.samples *.fa
 
-#echo "align finalized sequences ..."
-#cd $LOCATION/3_"$NAME"_alignment_and_cluster/Derep_alignment
-#mkdir temp
-##split -l 100 all_seqs_derep.fasta
-#cmalign -o temp/temp_aln.stk -g --noprob $MODEL_16S < all_seqs_derep.fasta   
-#java -Xmx10g -jar $ALIGNMENTTOOLS alignment-merger temp all_seqs_derep_aln.fa
+echo "align finalized sequences ..."
+mkdir $LOCATION/3_"$NAME"_alignment_and_cluster $LOCATION/3_"$NAME"_alignment_and_cluster/temp
+cd $LOCATION/3_"$NAME"_alignment_and_cluster
+$CMALIGN -o temp/temp_aln.stk -g --noprob $MODEL_16S $LOCATION/2_"$NAME"_uchime/chime_ref/all_seqs_unique_nospace_sorted.ref.good.fasta
+java -Xmx24g -jar $ALIGNMENTTOOLS alignment-merger temp all_seqs_derep_aln.fa >> $LOCATION/log.txt
 
-#echo "Calculating distance matrix ..."
-#mkdir $LOCATION/3_"$NAME"_alignment_and_cluster/complete_linkage_cluster $LOCATION/3_"$NAME"_alignment_and_cluster/complete_linkage_cluster/distance_matrix
-#cd $LOCATION/3_"$NAME"_alignment_and_cluster/
-#java -Xmx24g -jar $CLUST dmatrix --id-mapping Derep_alignment/all_seqs.ids --in Derep_alignment/all_seqs_derep_aln.fa --outfile complete_linkage_cluster/distance_matrix/all_seqs_derep_aligned.fasta_matrix.bin --mask "#=GC_RF" -l 25 --dist-cutoff $DIST
+echo "Calculating distance matrix ..."
+mkdir $LOCATION/3_"$NAME"_alignment_and_cluster/complete_linkage_cluster $LOCATION/3_"$NAME"_alignment_and_cluster/complete_linkage_cluster/distance_matrix
+cd $LOCATION/3_"$NAME"_alignment_and_cluster/
+java -Xmx24g -jar $CLUST dmatrix --id-mapping $LOCATION/2_"$NAME"_uchime/derep/all_seqs.ids --in all_seqs_derep_aln.fa --outfile complete_linkage_cluster/distance_matrix/all_seqs_derep_aligned.fasta_matrix.bin --mask "=GC_RF" -l 25 --dist-cutoff $DIST >> $LOCATION/log.txt
 
-#echo "Performing complete linkage clustering ..."
-#java -Xmx24g -jar $CLUST cluster --method complete --id-mapping Derep_alignment/all_seqs.ids --sample-mapping Derep_alignment/all_seqs.samples --dist-file complete_linkage_cluster/distance_matrix/all_seqs_derep_aligned.fasta_matrix.bin --outfile complete_linkage_cluster/all_seqs_derep_aligned.fasta_complete.clust --step $STEP
+echo "Performing complete linkage clustering ..."
+java -Xmx24g -jar $CLUST cluster --method complete --id-mapping $LOCATION/2_"$NAME"_uchime/derep/all_seqs.ids --sample-mapping $LOCATION/2_"$NAME"_uchime/derep/all_seqs.samples --dist-file complete_linkage_cluster/distance_matrix/all_seqs_derep_aligned.fasta_matrix.bin --outfile complete_linkage_cluster/all_seqs_derep_aligned.fasta_complete.clust --step $STEP >> $LOCATION/log.txt
 
-#echo "Generating OTU table ..."
-#mkdir $LOCATION/R
-#cd $LOCATION
-#java -Xmx24g -jar $CLUST cluster_to_Rformat 3_"$NAME"_alignment_and_cluster/complete_linkage_cluster/all_seqs_derep_aligned.fasta_complete.clust R/ $STEP $DIST
+#echo "mapping all sequences back to good dereped and chimera checked sequences of individual samples ..."
+#java -Xmx4g -jar $CLUST explode-mappings --out-dir explode-mappings derep/all_seqs.ids derep/all_seqs.samples chime_ref/all_seqs_unique_nospace_sorted.ref.good.fasta
 
-echo "Classify representative sequences ..."
+echo "Generating OTU table ..."
+mkdir $LOCATION/R
+cd $LOCATION
+java -Xmx24g -jar $CLUST cluster_to_Rformat 3_"$NAME"_alignment_and_cluster/complete_linkage_cluster/all_seqs_derep_aligned.fasta_complete.clust R/ $STEP $DIST >> $LOCATION/log.txt
+
+echo "Get representative sequences and determine their taxanomy ..."
 cd $LOCATION/3_"$NAME"_alignment_and_cluster/complete_linkage_cluster
-#java -Xmx4g -jar $CLUST rep-seqs --id-mapping ../Derep_alignment/all_seqs.ids --one-rep-per-otu all_seqs_derep_aligned.fasta_complete.clust $DIST ../Derep_alignment/all_seqs_derep_aln.fa
-#java -Xmx4g -jar $CLASSIFIER classify -c $BOOTSTRAP_VALUE -f fixrank -o ../../R/otu_taxa_fixrank.txt all_seqs_derep_aligned.fasta_complete.clust_rep_seqs.fasta 
-echo "Classify each otu ..."
-java -Xmx4g -jar $CLUST rep-seqs -c --id-mapping $LOCATION/3_"$NAME"_alignment_and_cluster/Derep_alignment/all_seqs.ids --one-rep-per-otu $LOCATION/3_"$NAME"_alignment_and_cluster/complete_linkage_cluster/all_seqs_derep_aligned.fasta_complete.clust $DIST $LOCATION/3_"$NAME"_alignment_and_cluster/Derep_alignment/all_seqs_derep_aligned.fasta
+java -Xmx24g -jar $CLUST rep-seqs -c --id-mapping $LOCATION/2_"$NAME"_uchime/derep/all_seqs.ids --one-rep-per-otu all_seqs_derep_aligned.fasta_complete.clust $DIST ../all_seqs_derep_aln.fa >> $LOCATION/log.txt
+java -Xmx24g -jar $CLASSIFIER classify -c $BOOTSTRAP_VALUE -f fixrank -o ../../R/otu_taxa_fixrank.txt all_seqs_derep_aligned.fasta_complete.clust_rep_seqs.fasta >> $LOCATION/log.txt
   
-### biom format ####
-#echo "Generating biom format file for R ..."
-#echo "Converting otu table to biom ..."
-#mkdir $LOCATION/3_"$NAME"_alignment_and_cluster/R
-#cd $LOCATION/3_"$NAME"_alignment_and_cluster/R
-#java -Xmx2g -jar $CLUST cluster-to-biom $LOCATION/3_"$NAME"_alignment_and_cluster/complete_linkage_cluster/all_seqs_derep_aligned.fasta_complete.clust $DIST > $LOCATION/3_"$NAME"_alignment_and_cluster/R/all_complete.clust.biom
+echo "Extract representative sequence alignment region ..."
+mkdir $LOCATION/3_"$NAME"_alignment_and_cluster/tree
+cd $LOCATION/3_"$NAME"_alignment_and_cluster/tree
+java -Xmx24g -jar $CLUST derep -f -o all_complete.clust_rep_seqs_modelonly.fasta rep_seqs.ids rep_seqs.sample ../all_seqs_derep_aligned.fasta_complete.clust_rep_seqs.fasta >> $LOCATION/log.txt
 
-#echo "Classify each otu ..."
-#java -Xmx4g -jar $CLUST rep-seqs -c --id-mapping $LOCATION/3_"$NAME"_alignment_and_cluster/Derep_alignment/all_seqs.ids --one-rep-per-otu $LOCATION/3_"$NAME"_alignment_and_cluster/complete_linkage_cluster/all_seqs_derep_aligned.fasta_complete.clust $DIST $LOCATION/3_"$NAME"_alignment_and_cluster/Derep_alignment/all_seqs_derep_aligned.fasta
-
-#java -Xmx4g -jar $CLASSIFIER classify -c $BOOTSTRAP_VALUE -f biom -m $LOCATION/3_"$NAME"_alignment_and_cluster/R/all_complete.clust.biom
-
-#echo "Constructing phylogenetic tree ..."
-#cd $LOCATION/3_"$NAME"_alignment_and_cluster/R
-#java -Xms4g -jar $CLUST derep -f -o all_complete.clust_rep_seqs_modelonly.fasta rep_seqs.ids rep_seqs.sample all_complete.clust_rep_seqs.fasta
-
-#$FASTTREE -nt -gtr < all_complete.clust_rep_seqs.fasta > all_complete.clust_rep_seqs.nwk
+echo "Constructing phylogenetic tree ..."
+$FASTTREE -nt -gtr < all_complete.clust_rep_seqs_modelonly.fasta > all_complete.clust_rep_seqs.nwk >> $LOCATION/log.txt
 
--- parsers/ncbi_xml_parser_get_na_aa.py
+++ parsers/ncbi_xml_parser_get_na_aa.py
@@ -4,6 +4,12 @@ import re
 
 f = sys.argv[1]
 
+def rename(name):
+	for ch in [' ', ',', '(', ')']:
+		if ch in name:
+			name_new = name.replace(ch, "_")
+	return name_new
+
 title = []
 na_seq = []
 aa_seq = []
@@ -11,7 +17,8 @@ for line in open(f, 'rU'):
 	line = line.strip()
 	if line.startswith("<Seqdesc_title>"):
 		ID = re.split(">|<", line)[2]
-		title.append(ID)
+		newID = rename(ID)
+		title.append(newID)
 	if line.startswith("<IUPACna>"):
 		na = re.split(">|<", line)[2]
 		na_seq.append(na)
@@ -22,22 +29,22 @@ for line in open(f, 'rU'):
 d_na = dict(zip(title, na_seq))
 d_aa = dict(zip(title, aa_seq))
 
-
 na_out = "%s.na.fa" % f.split(".")[0]
 na_f = open(na_out, 'w')
 for na_key in d_na:
 	if "lactamase" in na_key:
 #		print na_key
-		if len(d_na[na_key]) >= 900:
+		if len(d_na[na_key]) >= 900 and len(d_na[na_key]) <= 1500:
 			na_f.write('>%s_%s\n' % (f.split(".")[0], na_key))
 			na_f.write('%s\n' % d_na[na_key])
 
 aa_out = "%s.aa.fa" % f.split(".")[0]
 aa_f = open(aa_out, 'w')
+l = []
 for aa_key in d_aa:
 	if "lactamase" in aa_key:
 #		print aa_key
-		if len(d_aa[aa_key]) >= 300:
+		if len(d_aa[aa_key]) >= 300 and len(d_na[na_key]) <= 500:
 			aa_f.write('>%s_%s\n' % (f.split(".")[0], aa_key))
 			aa_f.write('%s\n' % d_aa[aa_key])
 
