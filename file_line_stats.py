5a13e3fd4ac5df1cdd5cd519e93a8cc42425ce03
Ubuntu <ubuntu@hi-mem-doe-ii.novalocal>
2015-11-24 20:36:24 +0000

    python script to calculate basic line stats in a file

---
+++ file_line_stats.py
@@ -0,0 +1,15 @@
+import sys
+import re
+import os
+
+files = sys.argv[1]
+
+def line_stats(bind_ss):
+	l = []
+	for lines in bind_ss:
+		line = lines.strip().split('\t')
+		l.append(len(line))
+	return 'max line length: ' + str(max(l)), 'min line length: ' + str(min(l))
+
+print line_stats(open(f, 'rU'))
+
