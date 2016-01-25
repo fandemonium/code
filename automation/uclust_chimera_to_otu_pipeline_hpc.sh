fa89431377cddb578b5a3775b0e7f910e6954ef6
Fan Yang <yangfan1@dev-intel14-phi.i>
2016-01-25 09:22:08 -0500

    updated cdhit clustering method 16s scripts for msu hpc

---
+++ automation/uclust_chimera_to_otu_pipeline_hpc.sh
@@ -0,0 +1,59 @@
+UCLUST="/mnt/research/rdp/public/thirdParty/usearch8.1.1831_i86linux64"
+DIR="/mnt/scratch/yangfan1/pitfoaming/Processed_16s"
+
+#cd DIR/2_quality_check
+#mkdir chimera_removal chimera_removal/derep chimera_removal/sorted chimera_removal/de_novo chimera_removal/ref chimera_removal/ref/stats chimera_removal/ref/chimeras chimera_removal/ref/good_otus good_seqs
+
+#cd DIR/2_quality_check/fastq_q25
+#mkdir DIR/2_quality_check/fasta_q25
+#for i in *.fastq; do python ~/code/fastq_to_fasta.py $i ../fasta_q25/$i.fasta; done
+#for name in *_L001_assembled.fastq_q25_q25.fastq.fasta; do mv -i "${name}" "${name/_L001_assembled.fastq_q25_q25.fastq.fasta/_q25.fastq.fasta}"; done
+#cd DIR/2_quality_check
+#cat fasta_q25/*.fasta > all_combined_q25.fa
+
+#/mnt/research/rdp/public/thirdParty/usearch8.1.1831_i86linux64 -derep_fulllength all_combined_q25.fa -fastaout all_combined_q25_unique.fasta -sizeout
+
+/mnt/research/rdp/public/thirdParty/usearch8.1.1831_i86linux64 -sortbysize all_combined_q25_unique.fasta -fastaout all_combined_q25_sorted.fa -minsize 2
+
+#cd /mnt/data3/fan/pitfoaming/chimera_removal/sorted
+#for i in *.fa; do usearch -cluster_otus $i -otu_radius_pct 3.0 -otus /mnt/data3/fan/pitfoaming/chimera_removal/de_novo/"$i"_otu1.fa; done
+
+#cd /mnt/data3/fan/pitfoaming/chimera_removal/de_novo
+#for i in *_otu1.fa; do usearch -uchime_ref $i -db /mnt/data3/qiime-db/gold.fa -uchimeout /mnt/data3/fan/pitfoaming/chimera_removal/ref/stats/"$i".uchime -strand plus -selfid -mindiv 1.5 -mindiffs 5 -chimeras /mnt/data3/fan/pitfoaming/chimera_removal/ref/chimeras/"$i"_chimera.fa -nonchimeras /mnt/data3/fan/pitfoaming/chimera_removal/ref/good_otus/"$i"_otu2.fa; done
+## new one:
+for i in *.fa; do usearch -uchime_ref $i -db /mnt/data3/fan/RDPinfernal1.1Traindata/RDPClassifier_16S_trainsetNo15_rawtrainingdata/trainset15_092015.fa -strand plus -chimeras /mnt/data3/fan/pitfoaming/2_rdp_uchime/chime_ref/chimeras/"$i"_chimera.fa -nonchimeras /mnt/data3/fan/pitfoaming/2_rdp_uchime/chime_ref/ref_good/"$i"_ref_good.fa; done
+
+###################################################################################
+## if you want to use modeled alignment to pick otu (e.g., rdp tools), do this:  ##
+###################################################################################
+#cd /mnt/data3/fan/pitfoaming/all_pool_quality_trimmed
+#for i in *.5.fasta; do usearch  -usearch_global $i -db /mnt/data3/fan/pitfoaming/chimera_removal/ref/good_otus/"$i"_unique.fasta_sorted.fa_otu1.fa_otu2.fa -strand plus -id 0.985 -matched /mnt/data3/fan/pitfoaming/good_seqs/"$i"_finalized.fa; done
+
+###################################################################################
+## if use uclust/uparse commands, generate master otus directly, do this:        ##
+###################################################################################
+#mkdir /mnt/data3/fan/pitfoaming/chimera_removal/master_otus
+#cd /mnt/data3/fan/pitfoaming/chimera_removal/master_otus
+#cat /mnt/data3/fan/pitfoaming/chimera_removal/ref/good_otus/*.fa > combined_good_otus.fa
+#usearch -derep_fulllength combined_good_otus.fa -fastaout combined_good_otus.fa_derep.fa -sizeout
+#usearch -sortbysize combined_good_otus.fa_derep.fa -fastaout combined_good_otus.fa_derep.fa_sorted.fa -minsize 1
+#usearch -cluster_otus combined_good_otus.fa_derep.fa_sorted.fa -otu_radius_pct 3.0 -uparse_break -100.0 -otus final_otus.fa -relabel OTU_
+
+#mkdir /mnt/data3/fan/pitfoaming/final_mapping /mnt/data3/fan/pitfoaming/final_mapping/mapping
+#cd /mnt/data3/fan/pitfoaming/all_pool_quality_trimmed
+#for i in *.5.fasta; do usearch -usearch_global $i -db /mnt/data3/fan/pitfoaming/chimera_removal/master_otus/final_otus.fa -strand plus -id 0.985 -uc /mnt/data3/fan/pitfoaming/final_mapping/mapping/$i.mapped.uc; done
+
+#usearch -utax /mnt/data3/fan/pitfoaming/chimera_removal/master_otus/final_otus.fa -db /mnt/data3/qiime-db/rdp_16s.fa -taxconfs /mnt/data3/qiime-db/rdp_16s.tc -strand both -utaxout /mnt/data3/fan/pitfoaming/final_mapping/otu_taxa.txt
+
+#cd /mnt/data3/fan/pitfoaming/final_mapping/mapping/
+#mkdir /mnt/data3/fan/pitfoaming/final_mapping/add_sample_names
+#for i in *.uc; do python /mnt/data3/fan/code/usearch_map_uc_parser.py $i > ../add_sample_names/"$i".sample; done
+#cat /mnt/data3/fan/pitfoaming/final_mapping/add_sample_names/*.sample > /mnt/data3/fan/pitfoaming/final_mapping/combined_samples_with_file_names.txt
+
+python /mnt/data3/fan/code/uclust_py/uc2otutab.py /mnt/data3/fan/pitfoaming/final_mapping/combined_samples_with_file_names.txt > /mnt/data3/fan/pitfoaming/final_mapping/otu_table.txt
+
+###################################################################################
+## if use cd-hit, do this:        ##
+###################################################################################
+## use rdp dereplicated sequences
+cdhit-est -i combined_renamed_seqs.fa -o combined_renamed_seqs_cdhit.fasta -c 0.97 -M 8000 -T 3
---
+++ cdhit_clustering/cdhit_clustering_pipeline_hpc.sh
@@ -0,0 +1,41 @@
+DIR="/mnt/scratch/yangfan1/pitfoaming/Processed_16s"
+CODE="/mnt/home/yangfan1/code/cdhit_clustering"
+RDPTOOLS="/mnt/research/rdp/public/RDPTools/"
+#### continue from after chimera check and remapping sequences: for i in *.fasta; do /mnt/research/rdp/public/thirdParty/usearch8.1.1831_i86linux64 -usearch_global $i -db ../chimera_check/all_combined_q25_otu1_ref_good_otu2.fa -strand plus -id 0.985 -matched ../../3_cdhit_clustering/quality_seqs/"$i"_finalized.fa; done"
+
+#cd $DIR/3_cdhit_clustering
+#mkdir renamed_seqs master_otus R
+##renameing sequences
+#cd $DIR/3_cdhit_clustering/quality_seqs
+#for i in *.fa; do python $CODE/renaming_seq_w_short_sample_name.py $i ../renamed_seqs/"$i".renamed.fa; done
+
+#concatenate everything
+cd $DIR/3_cdhit_clustering/renamed_seqs
+cat *.fa > ../master_otus/combined_renamed_seqs.fa
+
+#clustering
+module load CDHIT/4.6.1
+cd $DIR/3_cdhit_clustering/master_otus
+cd-hit-est -i combined_renamed_seqs.fa -o combined_renamed_seqs_cdhit.fasta -c 0.97 -M 8000 -T 3
+
+#make otu table
+python $CODE/cdhit_clstr_to_otu.py combined_renamed_seqs_cdhit.fasta.clstr > cdhit_otu_table_long.txt
+
+#convert otu table to wide format (otus as rows, samples as columns)
+module load R/2.15.1
+Rscript $CODE/convert_otu_table_long_to_wide_format.R cdhit_otu_table_long.txt cdhit_otu_table_wide.txt
+
+#identify otu taxonomy
+module load Java/1.7.0_51
+java -Xmx24g -jar $RDPTOOLS/classifier.jar classify -c 0.5 -f filterbyconf -o cdhit_otu_taxa_filterbyconf.txt -h cdhit_otu_taxa_filterbyconf_hierarchy.txt combined_renamed_seqs_cdhit.fasta
+
+#map otus to repseqs for rdp classified filterbyconf taxa table
+python $CODE/rep_seq_to_otu_mapping.py combined_renamed_seqs_cdhit.fasta.clstr > combined_renamed_seqs_cdhit_rep_seq_to_cluster.map
+Rscript $CODE/renaming_taxa_rep_seq_to_otus.R cdhit_otu_taxa_filterbyconf.txt combined_renamed_seqs_cdhit_rep_seq_to_cluster.map ../R/cdhit_taxa_table_w_repseq.txt
+
+#env file from Angela kent need to be mapped to the otu and taxa
+##1. create the mapping file
+#python $CODE/16s_fname_to_env_fname_to_sample_name.py all_16s_sample_names.txt > 16s_sample_name_to_env_table.map
+##2. merging in R
+## make taxa table, conver rep sequence to otu.
+Rscript ~/code/cdhit_clustering/renaming_taxa_rep_seq_to_otus.R cdhit_otu_taxa_filterbyconf.txt combined_renamed_seqs_cdhit_rep_seq_to_cluster.map ../R/taxa_table.txt`
