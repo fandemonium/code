daaca7e3b1b85fff34ac5b87e1d55a133b1ba74b
Fan Yang <fanyang@fans-mbp.eeob.iastate.edu>
2016-04-01 13:20:33 -0500

    split out refsoil_rast and R into their own repo

--- R/convert_datframe_to_list_by_row.R
+++
@@ -1,9 +0,0 @@
-f<-function(x){
-	phylum <- row.names(x)
-	oid <- x[1]
-	l <- rbind(as.vector(unlist(strsplit(oid, ",")), mode="list"))
-	as.character(l)
-}
-
-## to use:
-test2<-apply(test1, 1, f)
--- R/subset_list_from_multiple_columns.md
+++
@@ -1,1 +0,0 @@
-result.m<-final_results[final_results$Var1 %in% measurements$genus |final_results$Var2 %in% measurements$genus, ]
