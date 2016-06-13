f222143c2353ee3c11ad811760559c58a813c2c2
Ubuntu <ubuntu@hi-mem-doe-ii.novalocal>
2016-06-13 02:45:30 +0000

    ardb annotation file parser

--- parsers/ardb_anno_parser.py
+++ parsers/ardb_anno_parser.py
@@ -5,20 +5,29 @@ import os
 import re
 from itertools import groupby
 
-def group_matches(ardb_anno):
-	f = open(ardb_anno)
-	citer = (x[1] for x in groupby(f, lambda line: line[0] == ">"))
-	anno = {}
-	for header in citer:
-		mock_id = header.next()[1:].strip().split(">")[0]
-		for lines in citer.next():
-			string = lines.strip()
-			fields = string#[0]
-			anno[mock_id] = [fields]
-		yield anno
+l = []
+for n, lines in enumerate(open(sys.argv[1], 'rU')):
+	if n == 0:
+		continue
+	else:
+		line = lines.strip().split("\t")
+		mock_id = line[0]
+		l.append(mock_id)
 
-ardb_anno_f = sys.argv[1]
-d = group_matches(ardb_anno_f)
-for item in d:
-	print d[item]	
-
+with open(sys.argv[2]) as anno:
+	d = {}
+	groups = (x[1] for x in groupby(anno, lambda line: line[0] == ">"))
+#	groups = groupby(anno, lambda x: x.startswith(">"))
+	for k in groups:
+		k = k.next()[1:].strip()
+		data = []
+		for line in groups.next():
+#			print line
+			if line.strip() != "":
+				v = line.strip().split("\t")
+				data.append(v)		
+		d[k] = data
+			
+for item in l:
+	if item in d:
+		print item + "\t" + d[item][0][1]
