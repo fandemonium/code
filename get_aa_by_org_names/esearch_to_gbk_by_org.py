9f481771dbcb72b1b55e69b743e992b26489d508
Fan Yang <snisiarc@gmail.com>
2016-06-21 07:58:08 -0500

    updated esearch command

--- get_aa_by_org_names/esearch_to_gbk_by_org.py
+++ get_aa_by_org_names/esearch_to_gbk_by_org.py
@@ -4,6 +4,16 @@ from Bio import Entrez
 
 Entrez.email = "fyang@iastate.edu"
 
+l = []
 for lines in open(sys.argv[1],'rU'):
-	
-handle = Entrez.esearch(db="genome', term=
+	lines = lines.replace('"', '').strip().split("\t")
+	org = lines[2]
+	if org in l:
+		continue
+	else:
+		l.append(org)
+
+for item in l:	
+	handle = Entrez.esearch(db="genome", term=item)
+	record = Entrez.read(handle)
+	print record
