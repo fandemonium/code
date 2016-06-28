55367b19230a918b67dfab3b0f1e9778e189e304
Fan <snisiarc@gmail.com>
2016-06-28 10:55:14 -0400

    updated cdhit_clustering/renaming_taxa_rep_seq_to_otus.R with flexible length

--- cdhit_clustering/renaming_taxa_rep_seq_to_otus.R
+++ cdhit_clustering/renaming_taxa_rep_seq_to_otus.R
@@ -4,7 +4,7 @@ args<-commandArgs(TRUE)
 
 taxa<-read.delim(args[1], sep='\t', header=T, row.names=1)
 repseq<-rownames(taxa)
-tax_table<-cbind(repseq, taxa[, 1:6])
+tax_table<-cbind(repseq, taxa[, 1:length(taxa[1, ])])
 
 otu_map<-read.delim(args[2], sep='\t', header=F)
 names(otu_map)<-c("OTUS", "repseq")
