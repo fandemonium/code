3282c0b9ab53d964a332dfc5639f929a79dabf95
Fan <snisiarc@gmail.com>
2016-07-21 16:47:41 -0400

    updated the gene feature parser for missing gene types

--- parsers/gene_feature_parser.py
+++ parsers/gene_feature_parser.py
@@ -1,17 +1,27 @@
+### usage: python ~/repos/code/parsers/gene_feature_parser.py ecoli_salm_gene_features.txt > ecoli_salm_all_gene_features_w_positions.txt
 import sys
 import re
 
-for lines in open(sys.argv[1], 'rU'):
-	line = lines.strip().split(" ")
-	acc = re.split("\||\.3", line[0])[1]
-	gene = re.split("=|\]", line[1])[1]
-	locus_tag = re.split("=|\]", line[2])[1]
-	if "complement" in line[-1]:
+def get_positions(feature):
+	if "complement" in feature:
 		start = re.split("\(|\.\.|\)", line[-1])[1]
 		end = re.split("\(|\.\.|\)", line[-1])[2]
-		print "%s\t%s\t%s\t%s\t%s\t%s" % (acc, gene, locus_tag, start, end, "-")
+		return "%s\t%s\t%s" % (start, end, "-")
 	else:
 		start = re.split("=|\.\.|\]", line[-1])[1]
 		end = re.split("=|\.\.|\]", line[-1])[2]
-		print "%s\t%s\t%s\t%s\t%s\t%s" % (acc, gene, locus_tag, start, end, "+")
+		return "%s\t%s\t%s" % (start, end, "+")
 
+	
+for lines in open(sys.argv[1], 'rU'):
+	line = lines.strip().split(" ")
+	if len(line) == 4:
+		acc = re.split("\||\.3", line[0])[1]
+		gene = re.split("=|\]", line[1])[1]
+		locus_tag = re.split("=|\]", line[2])[1]
+		print "%s\t%s\t%s\t%s" % (acc, gene, locus_tag, get_positions(line[-1]))
+	else:
+		acc = re.split("\||\.3", line[0])[1]
+		locus_tag = re.split("=|\]", line[1])[1]
+		print "%s\t%s\t%s\t%s" % (acc, "NA", locus_tag, get_positions(line[-1]))
+		
