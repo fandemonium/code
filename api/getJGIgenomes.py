52babe57e1ca315b9328129089d1156609c46f42
Fan Yang <fanyang@fans-mbp.eeob.iastate.edu>
2016-07-27 11:33:42 -0500

    added shane's img api script

---
+++ api/getJGIgenomes.py
@@ -0,0 +1,69 @@
+## authored by Shane Dooley
+#python getJGIgenomes.py $JGI_Export_File $Job_Error_Filename 
+## 1. obtain an account from img.jgi.doe.gov/m/
+## 2. change the username and password in the getJGIgenomes.py
+## 3. From IMG browser, select all the genomes you want, add to cart, check the field "IMG Genome ID" to be included in the dispaly, and lastly import the excel sheet.
+## 4. Open the file in excel and make IMG Genome ID as the first column, save the file as a tab-delimted text file, say JGIgenomelist.txt.
+## 5. cmd: python getJGIgenomes.py JGIgenomelist.txt errlog.txt
+
+
+import urllib
+import urllib2
+import cookielib
+import sys
+import os
+
+filename = 'cookie.txt'
+cookie = cookielib.MozillaCookieJar(filename)
+handler = urllib2.HTTPCookieProcessor(cookie)
+opener = urllib2.build_opener(handler)
+post = urllib.urlencode({'login':'fyang@iastate.edu',
+                         'password':'####',
+                         'utf8':'%E2%9C%93',
+                         'commit':'Sign+In'})
+response = opener.open('https://signon.jgi.doe.gov/signon/create', post)
+cookie.save(ignore_discard=True, ignore_expires=True)
+#response = opener.open('https://signon.jgi.doe.gov/contact_updated')
+
+IMG_File = open(sys.argv[1],"r")
+errorFile = open(sys.argv[2],"w")
+header = IMG_File.next()
+header = header.strip()
+header = header.split('\t')
+listSize = 32000 #Temp variable just to keep track of where the download process is
+for line in IMG_File:
+  line = line.strip()
+  lineArr = line.split('\t')
+  folderName = "IMG_" + lineArr[0].strip()
+  if os.path.exists(folderName):
+	print "%s already exists, skip" %(folderName)
+	errorFile.write("%s already exists, skip\n" %(folderName))
+        continue
+  os.mkdir(folderName)
+
+  infoFileName = folderName+"/"+folderName+".info"
+  infoFileHandle = open(infoFileName,'w')
+  for i in range(0,len(lineArr)):
+    infoFileHandle.write(header[i]+": "+ lineArr[i] + "\n")
+  infoFileHandle.close()
+  try:
+    response = opener.open('http://genome.jgi.doe.gov/ext-api/downloads/get-directory?organism=IMG_'+lineArr[0])
+    if(response.read().find("download_bundle.tar.gz") == -1):
+      os.system("rm -rf " + folderName)
+      errorFile.write(folderName+": "+ "download bundle not found\n")
+      continue
+  except Exception as e:
+    os.system("rm -rf " + folderName)
+    errorFile.write(folderName+": "+ str(e)+ "\n")
+    continue
+    
+  downloadResponse = opener.open('http://genome.jgi.doe.gov/'+folderName+'/download/download_bundle.tar.gz')
+  saveDownloadbundle = open(folderName+"/"+folderName+"_downloadbundle.tar.gz","w")
+  saveDownloadbundle.write(downloadResponse.read())
+  print "Completed Downloading: %s %.2f" % (folderName,(float(len(os.listdir("./")))/listSize)*100)
+  #urllib2.HTTPError
+#response = opener.open('http://genome.jgi.doe.gov/ext-api/downloads/get-directory?organism=IMG_638154510')
+#print response.read()
+
+#savefile = open('cookieresult.gz', 'w')
+#savefile.write(response.read())
