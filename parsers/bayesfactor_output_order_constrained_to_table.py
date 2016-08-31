785a992983ec8f91701cc8967cf11801764b3ba3
Fan Yang <fanyang@fans-mbp.eeob.iastate.edu>
2016-08-31 14:09:32 -0500

    updated bf order constrained parser

--- parsers/bayesfactor_output_order_constrained_to_table.py
+++ parsers/bayesfactor_output_order_constrained_to_table.py
@@ -5,6 +5,9 @@ from itertools import groupby
 def group_separator(line):
 	return line.startswith("[1] \"Processing")
 
+def hasNumbers(input_string):
+	return any(char.isdigit() for char in input_string)
+
 f = open(sys.argv[1])
 
 #for lines in f:
@@ -22,7 +25,8 @@ for param, group in groupby(f, group_separator):
 				if lines.startswith("[1] "):
 					order = lines.strip().split("\"")[1]
 					d[col]=[order]
-				elif lines.startswith("   "):
+#				elif lines.startswith("   "): # for "sample_information_bayes_factors_order_restricted_H.txt"
+				elif hasNumbers(lines) is True: # for "bacteria_core_genus_bayes_factors_order_restricted_H.txt" and abov file
 					bf = lines.strip().split()[-1]
 					d[col].append(bf)
 			except:
