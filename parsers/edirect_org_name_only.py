5e0df41207a28cf745438a6588dad1c7d88b10f5
fyang3 <fyang3@ip-10-30-0-93.ec2.internal>
2017-11-03 03:59:43 +0000

    working with one column for edirect

---
+++ parsers/edirect_org_name_only.py
@@ -0,0 +1,10 @@
+import sys
+
+f = open(sys.argv[1], 'rU')
+for lines in f.readlines()[1:]:
+	line = lines.strip()#.split("\t")
+	genus = line.split(" ")[0]
+	species = line.split(" ")[1]
+	print "esearch -db assembly -query \'%s\' | elink -target nuccore | elink -target assembly | efetch -format docsum | xtract -pattern DocumentSummary -element FtpPath_GenBank |awk -F\"/\" \'{print $0\"/\"$NF\"_rna_from_genomic.fna.gz\"}\' | xargs wget -O %s.rna.fa.gz" % (line, genus + "_" + species)
+	#print "esearch -db assembly -query \'%s\' | elink -target nuccore | elink -target assembly | efetch -format docsum | xtract -pattern DocumentSummary -element FtpPath_RefSeq |awk -F\"/\" \'{print $0\"/\"$NF\"_rna_from_genomic.fna.gz\"}\' | xargs wget -O %s.rna.fa.gz" % (line, genus + "_" + species)
+
