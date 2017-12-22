d05b05e5c5102ab9c1152a334443d182c9d2b97b
fyang3 <fyang3@ip-10-30-0-93.ec2.internal>
2017-12-22 20:30:23 +0000

    added path

--- seq_util/fastq_to_fasta_w_input_names.py
+++ seq_util/fastq_to_fasta_w_input_names.py
@@ -8,9 +8,15 @@ if ".gz" in file_name:
 	f = gzip.open(file_name)
 else:
 	f = open(file_name, 'rU')
-	
+
+new_file_name = "_".join(file_name.strip().split("/"))
+out_name = new_file_name.split(".")[0] + ".fa"
+
+pw = sys.argv[2]
+out_path = pw + "/" + out_name
+
+out = open(out_path, 'w')
 for records in SeqIO.parse(f, "fastq"):
-	records.id = file_name.strip() + ":" + records.id.strip()
+	records.id = new_file_name + ":" + records.id.strip()
 #	print records.id
-#	SeqIO.write(records, out, 'fasta')
-	print ">" + records.id + '\n' + records.seq
+	SeqIO.write(records, out, 'fasta')
