16b200c1e6f98f9cf6f53fb15faf40bf2d65890f
fandemonium <snisiarc@gmail.com>
2018-07-05 16:40:52 -0700

    finished the blast to seq trunk

--- seq_util/parse_blast_get_aln_region.py
+++ seq_util/parse_blast_get_aln_region.py
@@ -1,5 +1,6 @@
 import sys
 from Bio import SeqIO
+from Bio.SeqFeature import SeqFeature, FeatureLocation
 
 blast = sys.argv[1]
 fa = sys.argv[2]
@@ -15,6 +16,9 @@ for lines in open(blast, 'rU'):
 	if int(aln_len) >= 900:
 		d[contigs] = [start, end]
 
-records.d = SeqIO.index(fa, "fasta")
+for records in SeqIO.parse(open(fa), "fasta"):
+	if records.id in d.keys():
+		print ">" + records.id + "_position_" + ":".join(d[records.id])
+		trunk = SeqFeature(FeatureLocation(int(d[records.id][0]), int(d[records.id][1])), type="gene", strand=1)
+		print records.seq[trunk.location.start:trunk.location.end]
 
-print records.d
