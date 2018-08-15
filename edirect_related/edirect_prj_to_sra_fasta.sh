1bd8de65aea4a8e8449ec811873e693cabd1dfd4
fandemonium <snisiarc@gmail.com>
2018-08-15 16:08:28 -0700

    adding options for pull raw pe files from sra

--- edirect_related/edirect_prj_to_sra_fasta.sh
+++ edirect_related/edirect_prj_to_sra_fasta.sh
@@ -1,2 +1,5 @@
 prj_id=1
 esearch -db sra -query $1 | efetch --format runinfo | cut -d ',' -f 1 | grep SRR | xargs fastq-dump --fasta
+
+## to get raw pair-ended reads:
+#esearch -db sra -query "PRJDB2050" | efetch --format runinfo | cut -d ',' -f 1 | grep DRR | xargs fastq-dump --split-files
